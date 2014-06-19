933991de4d72c9f91c590a92cf5a4cd1bdf26c74
Jeff Winkler <winkler1@gmail.com>
2014-06-19 17:15:28 -0400

    Base URL is only /cache/ in Production.

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     environment: environment,
-    baseURL: '/cache/',
+    baseURL: '',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -31,7 +31,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+     ENV.baseURL= '/cache/'
   }
 
   return ENV;
